516aa92b4477ace63e413b116e06a0529d13c7b1
iBacharnikov <cjba4ar@gmail.com>
2024-11-14 17:48:49 +0500

    try yo get sound loudness

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:flappy_bird/game/game.dart';
 import 'package:flutter/material.dart';
+import 'package:record/record.dart';
 
 class MainScreen extends StatefulWidget {
   final FlappyBirdGame game;
@@ -11,12 +15,42 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final TextEditingController nameC = TextEditingController(text: '');
+  final record = AudioRecorder();
+  late final Stream audioStream;
+
+  double calculateVolume(List<int> data) {
+    final buffer = Int16List.fromList(data); // Преобразуем данные в 16-битные целые
+    double sum = 0.0;
+
+    // Суммируем квадраты значений для RMS
+    for (var sample in buffer) {
+      sum += sample * sample;
+    }
+
+    // Рассчитываем среднеквадратичное значение (RMS)
+    double rms = sqrt(sum / buffer.length);
+    
+    // Нормализуем результат
+    double normalizedVolume = (rms / 32768).clamp(0.0, 1.0);
+    if (normalizedVolume > 0.006) {
+      print('CLICK!!!');
+    }
+    return normalizedVolume;
+  }
 
   @override
   void initState() {
     super.initState();
     widget.game.pauseEngine();
     nameC.addListener(() => setState(() {}));
+    setStream();
+  }
+
+  void setStream() async {
+    audioStream = await record.startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
+    audioStream.listen((data) {
+      print(calculateVolume(data));
+    });
   }
 
   @override
